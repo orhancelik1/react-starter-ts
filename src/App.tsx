ee59e380272f96bed2d46a718ec05891bd54ae1c
orhancelik1 <51966088+orhancelik1@users.noreply.github.com>
2023-02-01 21:37:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-red-600 text-2xl text-center underline mt-[10%]">
+      hello this is starter kit with ts + tailwind
     </div>
   );
 }
